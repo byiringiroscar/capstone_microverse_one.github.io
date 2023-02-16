cbaf645a345ddddc6dae8bb3bfa4544490ba49ad
Oscar Byiringiro <byiringiroscar>
2023-02-16 07:38:27 +0200

    fix linters and mobile body content

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const menuLogo = document.querySelector('#menu-logo');
-const menuClose = document.querySelector("#menu-close");
+const menuClose = document.querySelector('#menu-close');
 const mobileBody = document.querySelector('.mobile-body');
 
-menuLogo.addEventListener('click', function(){
-    mobileBody.classList.add('active')
+menuLogo.addEventListener('click', () => {
+  mobileBody.classList.add('active');
 });
 
-menuClose.addEventListener('click', function(){
-    mobileBody.classList.remove('active')
+menuClose.addEventListener('click', () => {
+  mobileBody.classList.remove('active');
 });
